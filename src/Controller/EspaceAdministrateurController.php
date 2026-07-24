abf199c5d71095a79c28e4b6e27d95d02c6e26e7
ByHaegang <gregoryteisseyre.studi@gmail.com>
2026-07-24 17:21:22 +0200

    Correction nouveau Controller

--- src/Controller/EspaceAdministrateurController.php
+++ src/Controller/EspaceAdministrateurController.php
@@ -9,3 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
+class EspaceAdministrateurController extends AbstractController {
+    
+}
