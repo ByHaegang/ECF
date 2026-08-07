7acafb9a20c186ffe7d5d9eb2337c72dc1339634
ByHaegang <gregoryteisseyre.studi@gmail.com>
2026-08-07 16:09:53 +0200

    Ajustement page Utilisateur et ajout de bouton deconnection avec sécurité jeton csrf

--- src/Controller/EspaceUtilisateurController.php
+++ src/Controller/EspaceUtilisateurController.php
@@ -74,47 +74,83 @@ class EspaceUtilisateurController extends AbstractController
     }
 
     #[Route('/espace_utilisateur/commande/{id}/modifier', name: 'app_modifier_commande', methods: ['POST'])]
-    public function modifierCommande(
-        int $id, 
-        Request $request, 
-        CommandeDAO $commandeDAO, 
-        UtilisateurDAO $utilisateurDAO
-    ): Response {
-        $session = $request->getSession();
-        $email = $session->get('user_email');
+public function modifierCommande(
+    int $id, 
+    Request $request, 
+    CommandeDAO $commandeDAO, 
+    UtilisateurDAO $utilisateurDAO
+): Response {
+    $session = $request->getSession();
+    $email = $session->get('user_email');
 
-        if (!$email) {
-            $this->addFlash('attention', 'Veuillez vous connecter pour effectuer cette action.');
-            return $this->redirectToRoute('app_login');
-        }
+    if (!$email) {
+        $this->addFlash('attention', 'Veuillez vous connecter pour effectuer cette action.');
+        return $this->redirectToRoute('app_login');
+    }
 
-        $utilisateur = $utilisateurDAO->getUtilisateurByEmail($email);
-        if (!$utilisateur) {
-            $session->clear();
-            return $this->redirectToRoute('app_login');
-        }
-        $utilisateurId = (int)$utilisateur['utilisateur_id'];
+    $utilisateur = $utilisateurDAO->getUtilisateurByEmail($email);
+    if (!$utilisateur) {
+        $session->clear();
+        return $this->redirectToRoute('app_login');
+    }
+    $utilisateurId = (int)$utilisateur['utilisateur_id'];
 
-        $datePrestation = $request->request->get('date_prestation');
-        $heureLivraison = $request->request->get('heure_livraison');
-        $nombrePersonne = (int)$request->request->get('nombre_personne');
-        $prixMenu       = (int)$request->request->get('prix_menu');
+    $commandeActuelle = $commandeDAO->getDetailCommande($id, $utilisateurId);
 
-        $modificationReussie = $commandeDAO->modifierCommandeParUtilisateur(
-            $id, 
-            $utilisateurId, 
-            $datePrestation, 
-            $heureLivraison, 
-            $nombrePersonne, 
-            $prixMenu
-        );
-
-        if ($modificationReussie) {
-            $this->addFlash('success', 'La commande #' . $id . ' a bien été modifiée.');
-        } else {
-            $this->addFlash('error', 'Impossible de modifier la commande. Vérifiez qu\'elle est toujours "En attente".');
-        }
-
+    if (!$commandeActuelle) {
+        $this->addFlash('error', 'Commande introuvable ou action non autorisée.');
         return $this->redirectToRoute('app_espace_utilisateur');
+    }
+
+    $datePrestation = $request->request->get('date_prestation');
+    $heureLivraison = $request->request->get('heure_livraison');
+    $nouveauNombrePersonne = (int)$request->request->get('nombre_personne');
+
+    $ancienPrix = (int)$commandeActuelle['prix_menu'];
+    $ancienNombrePersonne = (int)$commandeActuelle['nombre_personne'];
+    
+    $prixUnitaire = $ancienPrix / $ancienNombrePersonne;
+    $nouveauPrixMenu = (int) round($prixUnitaire * $nouveauNombrePersonne);
+
+    $modificationReussie = $commandeDAO->modifierCommandeParUtilisateur(
+        $id, 
+        $utilisateurId, 
+        $datePrestation, 
+        $heureLivraison, 
+        $nouveauNombrePersonne, 
+        $nouveauPrixMenu
+    );
+
+    if ($modificationReussie) {
+        $this->addFlash('success', 'La commande #' . $id . ' a bien été modifiée.');
+    } else {
+        $this->addFlash('error', 'Impossible de modifier la commande. Vérifiez qu\'elle est toujours "En attente".');
+    }
+
+    return $this->redirectToRoute('app_espace_utilisateur');
+}
+
+    #[Route('/deconnexion', name: 'app_deconnexion', methods: ['POST'])]
+public function logout(Request $request): Response
+    {
+    $session = $request->getSession();
+
+    if (!$session->has('user_email')) {
+        return $this->redirectToRoute('app_login');
+    }
+
+    $submittedToken = $request->request->get('_token');
+
+    if (!$this->isCsrfTokenValid('logout', $submittedToken)) {
+        $this->addFlash('attention', 'Jeton de sécurité invalide. Action annulée.');
+        return $this->redirectToRoute('app_espace_utilisateur');
+    }
+
+    $session->clear();
+    $session->invalidate();
+
+    $this->addFlash('success', 'Vous avez bien été déconnecté.');
+
+    return $this->redirectToRoute('app_home');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\DAO\AvisDAO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,5 +19,29 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'liste_avis' => $avis
         ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_deconnexion', methods: ['POST'])]
+public function logout(Request $request): Response
+    {
+    $session = $request->getSession();
+
+    if (!$session->has('user_email')) {
+        return $this->redirectToRoute('app_login');
+    }
+
+    $submittedToken = $request->request->get('_token');
+
+    if (!$this->isCsrfTokenValid('logout', $submittedToken)) {
+        $this->addFlash('attention', 'Jeton de sécurité invalide. Action annulée.');
+        return $this->redirectToRoute('app_home');
+    }
+
+    $session->clear();
+    $session->invalidate();
+
+    $this->addFlash('success', 'Vous avez bien été déconnecté.');
+
+    return $this->redirectToRoute('app_home');
     }
 }
